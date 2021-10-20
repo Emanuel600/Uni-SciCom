b924b5d61a3bf26fb97120eca961c2a03ae111fd
Emanuel <78040463+Emanuel600@users.noreply.github.com>
2021-10-20 16:56:13 -0300

    Update calc.h
    Mudança na ordem das funções para melhor legibilidade

--- Aula1_exemplos/calc.h
+++ Aula1_exemplos/calc.h
@@ -1,12 +1,6 @@
-/*
- * calc.h
- *
- *  Created on: 16 Oct 2021
- *      Author: desker
- */
-
 #ifndef CALC_H_
 #define CALC_H_
+
 /* Conjunto de funções que retornam !double! */
 // Calcula e^x para $alg algarismos
 double expon(double x, int alg);
@@ -14,13 +8,14 @@ double expon(double x, int alg);
 double coss(double ang, int alg);
 // Calcula raiz de x para $alg algarismos com valor inicial $inic
 double raiz(double x, int alg, double inic);
+// Calcula fatorial de n, espera números inteiros => Importante retornar double para divisões
+double fact(double n);
+
+/* Este conjunto retorna float para enfatizar erros de truncamento */
 /* Calcula soma do inverso dos quadrados de x termos */
 // Do maior para o menor
 float som_in_mai(int x);
 // Do menor para o maior
 float som_in_men(int x);
-
-// Calcula fatorial de n, espera números inteiros
-double fact(double n);
 
 #endif /* CALC_H_ */
